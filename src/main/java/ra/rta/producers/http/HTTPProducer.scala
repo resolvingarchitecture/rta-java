f28d0a0c978018373d5f23ff6d1c2272c66816fd
objectorange <objectorange@1m5.io>
2020-03-13 13:43:15 -0700

    move to messagemanager

--- src/main/java/ra/rta/producers/http/HTTPProducer.scala
+++ src/main/java/ra/rta/producers/http/HTTPProducer.scala
@@ -1,12 +1,11 @@
 package ra.rta.producers.http
 
 import java.io.BufferedReader
-import java.util.Properties
 
 import javax.servlet.ServletConfig
 import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
-import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerRecord}
 import org.slf4j.LoggerFactory
+import ra.rta.producers.MessageManager
 
 /**
  *
@@ -15,30 +14,11 @@ class HTTPProducer extends HttpServlet {
 
   val LOG = LoggerFactory.getLogger(classOf[HTTPProducer])
 
-  var transactionProducerProps: Properties = _
-  var transactionProducer: Producer[String, String] = _
-
-  var referenceProducerProps: Properties = _
-  var referenceProducer: Producer[String, String] = _
+  var messageManager: MessageManager = _
 
   override def init(servletConfig: ServletConfig): Unit = {
     super.init(servletConfig)
-    // Kafka Producers Setup
-    transactionProducerProps = new Properties
-    transactionProducerProps.put("metadata.broker.list", servletConfig.getInitParameter("metadata.broker.list"))
-    transactionProducerProps.put("serializer.class", "kafka.serializer.StringEncoder")
-    transactionProducerProps.put("key.serializer.class", "kafka.serializer.StringEncoder")
-    transactionProducerProps.put("request.required.acks", "1")
-    transactionProducerProps.put("partitioner.class", "com.segmint.rt.producers.utilities.partitioners.KafkaRoundRobinPartitioner")
-    transactionProducer = new KafkaProducer[String, String](transactionProducerProps)
-
-    referenceProducerProps = new Properties
-    referenceProducerProps.put("metadata.broker.list", servletConfig.getInitParameter("metadata.broker.list"))
-    referenceProducerProps.put("serializer.class", "kafka.serializer.StringEncoder")
-    referenceProducerProps.put("key.serializer.class", "kafka.serializer.StringEncoder")
-    referenceProducerProps.put("request.required.acks", "-1")
-    referenceProducerProps.put("partitioner.class", "com.segmint.rt.producers.utilities.partitioners.KafkaRoundRobinPartitioner")
-    referenceProducer = new KafkaProducer[String, String](referenceProducerProps)
+    messageManager = new MessageManager(servletConfig.getServletContext().)
   }
 
   override def doPost(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
@@ -53,13 +33,8 @@ class HTTPProducer extends HttpServlet {
       while ({line = reader.readLine; line} != null) sb.append(line)
       val body = sb.toString
       if(body!=null && !"".equals(body)) {
-        val message: ProducerRecord[String, String] = new ProducerRecord[String, String](topic, ""+System.currentTimeMillis(), body)
-        val producer: Producer[String, String] = topic match {
-          case "transaction" => transactionProducer
-          case "reference" => referenceProducer
-        }
-        try {// TODO: Getting HTTP 405 Error code but message gets sent and received by Kafka
-          producer.send(message)
+        try {
+          messageManager.send(topic, body, !topic.eq("transaction"))
           resp.setStatus(200)
           resp.setContentType("application/json")
           resp.getOutputStream.print("{test sat}")
