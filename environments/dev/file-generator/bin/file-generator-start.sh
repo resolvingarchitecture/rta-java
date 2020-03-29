2f4d4deae91fd3beba3f17c8e587c28050e269a9
objectorange <brian@resolvingarchitecture.io>
2020-03-28 22:59:41 -0700

    add kafka config

--- environments/dev/file-generator/bin/file-generator-start.sh
+++ environments/dev/file-generator/bin/file-generator-start.sh
@@ -3,4 +3,4 @@
 # Start Script for the File Producer
 # -----------------------------------------------------------------------------
 
-exec java -Xmx3G -cp /Library/file-producer/bin/file-producer.jar ra.rta.models.utilities.FileGenerator /Library/file-producer/data/incoming/partner/staging/ 1 1
+exec java -Xmx3G -cp /Library/file-producer/bin/file-producer.jar ra.rta.models.utilities.FinancialTransactionFileGenerator /Library/file-producer/data/incoming/group/staging/ 1 1
--- environments/staging/file-generator/bin/test-file-generator-start.sh
+++ environments/staging/file-generator/bin/test-file-generator-start.sh
@@ -3,4 +3,4 @@
 # Start Script for the File Producer
 # -----------------------------------------------------------------------------
 
-exec java -Xmx3G -cp /etl/bin/file-producer.jar ra.rta.models.utilities.FileGenerator /etl/incoming/partner/test/ 20000000 720
+exec java -Xmx3G -cp /etl/bin/file-producer.jar ra.rta.models.utilities.FinancialTransactionFileGenerator /etl/incoming/group/test/ 20000000 720
